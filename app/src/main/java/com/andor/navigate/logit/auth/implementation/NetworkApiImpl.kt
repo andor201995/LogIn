877f028ba5afed9ab01cd083061ee3a356c6e15c
Anmol Srivastava <andor201995@users.noreply.github.com>
2019-06-02 14:50:09 +0530

    init

---
+++ app/src/main/java/com/andor/navigate/logit/auth/implementation/NetworkApiImpl.kt
@@ -0,0 +1,10 @@
+package com.andor.navigate.logit.auth.implementation
+
+import com.andor.navigate.logit.core.api.NetworkApi
+
+class NetworkApiImpl : NetworkApi {
+    override fun initiateLogin(email: String, password: String) {
+
+
+    }
+}
---
+++ app/src/main/java/com/andor/navigate/logit/core/api/NetworkApi.kt
@@ -0,0 +1,5 @@
+package com.andor.navigate.logit.core.api
+
+interface NetworkApi {
+    fun initiateLogin(email: String, password: String)
+}
